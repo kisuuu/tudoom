d9c759b5fe3f732bfeda1e96c24650aba49e5253
kisuuu <kishan.poriya19310@gmail.com>
2022-06-10 18:54:51 +0530

    description

---
+++ lib/constants/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// const black = Colors.black;
+const loginButton = Color.fromARGB(255, 203, 203, 203);
+const loginHeaderText = Color.fromARGB(255, 88, 96, 116);
+const textfieldBorderColor = Color.fromARGB(255, 158, 158, 158);
+const rememberMeText = Color.fromARGB(255, 126, 123, 123);
+const purple = Color.fromARGB(255, 91, 66, 143);
+const creamWhite = Color.fromARGB(255, 252, 249, 245);
+const greyColor = Color.fromARGB(255, 79, 91, 91);
+const black = Color.fromARGB(255, 37, 48, 48);
+const white = Colors.white;
